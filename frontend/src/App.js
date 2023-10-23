9349f9952cbda0c3ff7e7412df95b2ea50ae0386
johnclinton14497 <130153828+johnclinton14497@users.noreply.github.com>
2023-10-23 14:34:25 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DataDisplay from './DataDisplay';
+
+function App() {
+  return (
+    <div className="App">
+      <DataDisplay />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ frontend/src/DataDisplay.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+
+function DataDisplay() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API
+    fetch('http://3.134.113.238:5000/api/data')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Data from Backend API</h1>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default DataDisplay;
+
